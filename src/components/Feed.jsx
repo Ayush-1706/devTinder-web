f160be5ced116381adb8f88e4dc0c4547df5747e
Ayush Shukla <ayushshukla50@gmail.com>
2025-03-26 18:11:16 +0530

    update import

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { addFeed } from "../utils/feedSlice";
 import { useDispatch, useSelector } from "react-redux";
-import UserCard from "./userCard";
+import UserCard from "./UserCard";
 
 const Feed = () => {
     const feed = useSelector((state) => state.feed);
